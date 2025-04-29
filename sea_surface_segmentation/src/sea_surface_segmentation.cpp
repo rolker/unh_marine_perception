92a4a295154920b104d4850b45d1acf38498b371
Field User <field@ccom.unh.edu>
2025-04-29 21:52:22 +0000

    Displays image, still need to decode segmentation

--- sea_surface_segmentation/src/sea_surface_segmentation.cpp
+++ sea_surface_segmentation/src/sea_surface_segmentation.cpp
@@ -1,51 +1,187 @@
 
 #include "rclcpp/rclcpp.hpp"
-#include "rclcpp_lifecycle/lifecycle_node.hpp"
+
+#include "depthai_bridge/BridgePublisher.hpp"
+#include "depthai_bridge/ImageConverter.hpp"
 
 
 #include "depthai/device/Device.hpp"
+#include "depthai/pipeline/datatype/NNData.hpp"
+#include "depthai/pipeline/node/ColorCamera.hpp"
+#include "depthai/pipeline/node/XLinkOut.hpp"
 #include "depthai/pipeline/Pipeline.hpp"
+#include "depthai/pipeline/node/NeuralNetwork.hpp"
 
-class SeaSurfaceSegmentation: public rclcpp_lifecycle::LifecycleNode
+#include "sensor_msgs/msg/image.hpp"
+
+
+void segmentationToRosMsg(const std::shared_ptr<dai::ADatatype>& data, std::deque<sensor_msgs::msg::Image>& outImageMsgs)
+{
+  auto in_det = std::dynamic_pointer_cast<dai::NNData>(data);
+
+  for(auto layer_name: in_det->getAllLayerNames())
+  std::cout << layer_name << std::endl;
+}
+
+void segmentationCallback(const std::string& /*name*/, const std::shared_ptr<dai::ADatatype>& data)
+{
+  std::cout << "segmentation callback" << std::endl;
+  auto in_det = std::dynamic_pointer_cast<dai::NNData>(data);
+
+  for(auto layer_name: in_det->getAllLayerNames())
+  std::cout << layer_name << std::endl;
+
+  auto layer_data = in_det->getLayerFp16("prediction");
+  std::cout << layer_data.size() << " data elements" << std::endl;
+  for(int i = 0; i < 10 && i < layer_data.size(); i++)
+    std::cout << layer_data[i] << ", ";
+  std::cout << std::endl;
+
+}
+
+class SeaSurfaceSegmentation: public rclcpp::Node
 {
 public:
   explicit SeaSurfaceSegmentation()
-  : rclcpp_lifecycle::LifecycleNode("sea_surface_segmentation")
+  : rclcpp::Node("sea_surface_segmentation")
   {
-
   }
 
-  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_configure(const rclcpp_lifecycle::State &)
+  void initialize()
   {
     declare_parameter("neural_network", std::string());
+    auto blob_path = get_parameter("neural_network").as_string();
+
+    std::string mx_id = "x";
+    declare_parameter("mx_id", mx_id);
+    mx_id = get_parameter("mx_id").as_string();
+
+    declare_parameter("frame_id", "camera_optical_frame");
+
+    std::string camera_name = "oak";
+    declare_parameter("camera_name", camera_name);
+    camera_name = get_parameter("camera_name").as_string();
+
+    pipeline_ = std::make_shared<dai::Pipeline>();
+
+    pipeline_->setOpenVINOVersion(dai::OpenVINO::VERSION_2021_4);
+    neural_network_ = pipeline_->create<dai::node::NeuralNetwork>();
+    neural_network_->setBlobPath(blob_path);
+    neural_network_->setNumPoolFrames(4);
+    neural_network_->input.setBlocking(false);
+    neural_network_->setNumInferenceThreads(2);
+
+    camera_ = pipeline_->create<dai::node::ColorCamera>();
+    camera_->setImageOrientation(dai::CameraImageOrientation::ROTATE_180_DEG);
+    camera_->setPreviewSize(512, 384);
+    camera_->setInterleaved(false);
+    camera_->setFps(5);
+    camera_->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
+
+    xlink_camera_ = pipeline_->create<dai::node::XLinkOut>();
+    xlink_camera_->setStreamName("camera");
+
+    xlink_neural_network_ = pipeline_->create<dai::node::XLinkOut>();
+    xlink_neural_network_->setStreamName("neural_network");
+
+    camera_->preview.link(neural_network_->input);
+    neural_network_->passthrough.link(xlink_camera_->input);
+    neural_network_->out.link(xlink_neural_network_->input);
+
+    auto available_devices = dai::Device::getAllAvailableDevices();
+    for(auto device_info: available_devices)
+    {
+      RCLCPP_INFO_STREAM(get_logger(), "Device Mx ID: " << device_info.getMxId());
+      if(device_info.getMxId() == mx_id)
+      {
+        if(device_info.state == X_LINK_UNBOOTED || device_info.state == X_LINK_BOOTLOADER)
+        {
+          device_ = std::make_shared<dai::Device>(*pipeline_, device_info);
+          break;
+        }
+        else if(device_info.state == X_LINK_BOOTED)
+        {
+          RCLCPP_ERROR_STREAM(get_logger(), "Device with MxID " << mx_id << " is already booted on a different process.");
+          throw std::runtime_error("\" DepthAI Device with MxId  \"" + mx_id + "\" is already booted on different process.  \"");
+        }
+      }
+      else if(mx_id == "x")
+      {
+        device_ = std::make_shared<dai::Device>(*pipeline_);
+      }
+    }
+
+    if(!device_)
+      throw std::runtime_error("\" DepthAI Device not found.  \"");
+
+    camera_queue_ = device_->getOutputQueue("camera", 5, false);
+    segmentation_queue_ = device_->getOutputQueue("neural_network", 5, false);
+ 
+    auto calibration_handler = device_->readCalibration();
+
+    auto frame_id = get_parameter("frame_id").as_string();
+    image_converter_ = std::make_shared<dai::rosBridge::ImageConverter>(frame_id, true);
+    auto camera_info = image_converter_->calibrationToCameraInfo(calibration_handler, dai::CameraBoardSocket::CAM_A, 512, 384);
 
 
-    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
-  }
+    auto node = shared_from_this();
 
-  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_activate(const rclcpp_lifecycle::State & state)
-  {
-    LifecycleNode::on_activate(state);
-    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
-  }
+    image_publisher_ = std::make_shared<dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame> >(
+      camera_queue_,
+      node,
+      camera_name+"/image_raw",
+      std::bind(&dai::rosBridge::ImageConverter::toRosMsg, image_converter_.get(), std::placeholders::_1, std::placeholders::_2),
+      10,
+      camera_info,
+      camera_name,
+      false
+    );
 
-  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_cleanup(const rclcpp_lifecycle::State &)
-  {
-    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+    image_publisher_->addPublisherCallback();
+
+    segmentation_queue_->addCallback(&segmentationCallback);
+
+    //auto segmentation_camera_info = segmentation_converter_->calibrationToCameraInfo(calibration_handler, dai::CameraBoardSocket::CAM_A, 128, 96);
+
+
+    // segmentation_publisher_ = std::make_shared<dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame> >(
+    //   segmentation_queue_,
+    //   node,
+    //   "segmentation_raw",
+    //   &segmentationToRosMsg,
+    //   10,
+    //   segmentation_camera_info,
+    //   camera_name,
+    //   false
+    // );
+
+    //segmentation_publisher_->addPublisherCallback();
+
   }
 
 private:
-  dai::Pipeline pipeline_;
-  dai::Device device_;
+  std::shared_ptr<dai::Pipeline> pipeline_;
+  std::shared_ptr<dai::node::NeuralNetwork> neural_network_;
+  std::shared_ptr<dai::node::ColorCamera> camera_;
+  std::shared_ptr<dai::node::XLinkOut> xlink_camera_;
+  std::shared_ptr<dai::node::XLinkOut> xlink_neural_network_;
+  std::shared_ptr<dai::DataOutputQueue> camera_queue_;
+  std::shared_ptr<dai::DataOutputQueue> segmentation_queue_;
+
+  std::shared_ptr<dai::Device> device_;
+  std::shared_ptr<dai::rosBridge::ImageConverter> image_converter_;
+  std::shared_ptr<dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame> > image_publisher_;
+
+  std::shared_ptr<dai::rosBridge::ImageConverter> segmentation_converter_;
+  std::shared_ptr<dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame> > segmentation_publisher_;
+
 };
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
   auto sss = std::make_shared<SeaSurfaceSegmentation>();
+  sss->initialize();
   rclcpp::executors::SingleThreadedExecutor exe;
   exe.add_node(sss->get_node_base_interface());
   exe.spin();
